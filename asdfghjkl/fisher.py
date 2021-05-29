5a629ab33c4dc3d9df57c8635ce4755f7beb85ea
kazukiosawa <osawa1021@gmail.com>
2021-05-29 18:41:48 +0200

    Add woodbury inverse-Fisher vector product

--- asdfghjkl/fisher.py
+++ asdfghjkl/fisher.py
@@ -1,4 +1,6 @@
 from contextlib import contextmanager
+import numpy as np
+
 import torch
 import torch.nn.functional as F
 from .core import extend
@@ -27,7 +29,8 @@ __all__ = [
     'zero_fisher',
     'zero_fvp',
     'fisher_for_cross_entropy_eigenvalues',
-    'fisher_free_for_cross_entropy'
+    'fisher_free_for_cross_entropy',
+    'woodbury_ifvp'
 ]
 
 _supported_types = [FISHER_EXACT, FISHER_MC, COV]
@@ -578,3 +581,41 @@ def _accumulate_fvp(module, src_attr, fisher_type, scale=1., accumulate=False):
 
 def _get_fvp_attr(fisher_type):
     return f'{fisher_type}_vp'
+
+
+def woodbury_ifvp(
+        vec,
+        model,
+        inputs,
+        targets,
+        loss_fn,
+        damping=1e-5,
+        data_average=True,
+        fisher_shape=None,
+):
+    """
+    Calculate inverse-empirical Fisher vector product by using the Woodbury matrix identity
+    """
+    assert damping > 0, 'Damping value has to be positive.'
+
+    with extend(model, OP_BATCH_GRADS):
+        model.zero_grad(set_to_none=True)
+        outputs = model(inputs)
+        loss = loss_fn(outputs, targets, reduction='sum')
+        loss.backward()
+
+        batch_g_all = []
+        for _, batch_g in _module_batch_flatten_grads(model):
+            batch_g_all.append(batch_g)
+        grads = torch.cat(batch_g_all, dim=1).T  # (p, n)
+
+    p, n = grads.shape
+    if data_average:
+        grads /= np.sqrt(n)
+    assert vec.shape == (p,)
+    gram = torch.matmul(grads.T, grads)  # (n, n)
+    inv = torch.inverse(gram + torch.eye(n) * damping)  # (n, n)
+    b = torch.matmul(inv, torch.matmul(grads.T, vec))
+
+    return (vec - torch.matmul(grads, b)) / damping  # (p,)
+
