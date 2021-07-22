83035d33090b510e415e0ab7f2dc8fb4b58fc5e8
kazukiosawa <osawa1021@gmail.com>
2021-07-22 21:23:09 +0200

    Add files for uploading package to PyPI

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
-from setuptools import find_packages, setup
+import setuptools
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
-setup(
-      name='asdfghjkl',
-      version='0.0.1',
-      description='ASDL: Automatic Second-order Differentiation (for Fisher, Gradient covariance, Hessian, Jacobian, and Kernel) Library',
-      install_requires=requirements,
-      packages=find_packages(include=['asdfghjkl', 'asdfghjkl.*']),
-      python_requires='>=3.6'
-      )
+if __name__ == "main":
+    setuptools.setup()
