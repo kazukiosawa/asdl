e5d7236c008886e07fd0d7ccb47d072b7f9cd261
Erik A. Daxberger <erik.daxberger@gmx.de>
2021-08-23 09:16:47 +0100

    Add cross entropy loss with soft targets for K-prior functional penalty

--- asdfghjkl/fr.py
+++ asdfghjkl/fr.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from contextlib import contextmanager
 
 import torch
@@ -86,7 +86,7 @@ class PastTask:
         mean = self.mean[idx] if idx is not None else self.mean  # (n, c)
 
         if use_kprior_penalty:
-            return F.cross_entropy(current_mean, mean)
+            return cross_entropy_with_probs(current_mean, mean)
 
         b = current_mean - mean     # (n, c)
         if kernel_inv is None:
@@ -105,6 +105,66 @@ class PastTask:
             b = b.flatten()  # (nc,)
 
         return torch.dot(b, v)
+
+
+def cross_entropy_with_probs(
+    input: torch.Tensor,
+    target: torch.Tensor,
+    weight: Optional[torch.Tensor] = None,
+    reduction: str = "mean",
+) -> torch.Tensor:
+    """Calculate cross-entropy loss when targets are probabilities (floats), not ints.
+    PyTorch's F.cross_entropy() method requires integer labels; it does accept
+    probabilistic labels. We can, however, simulate such functionality with a for loop,
+    calculating the loss contributed by each class and accumulating the results.
+    Libraries such as keras do not require this workaround, as methods like
+    "categorical_crossentropy" accept float labels natively.
+    Note that the method signature is intentionally very similar to F.cross_entropy()
+    so that it can be used as a drop-in replacement when target labels are changed from
+    from a 1D tensor of ints to a 2D tensor of probabilities.
+    Parameters
+    ----------
+    input
+        A [num_points, num_classes] tensor of logits
+    target
+        A [num_points, num_classes] tensor of probabilistic target labels
+    weight
+        An optional [num_classes] array of weights to multiply the loss by per class
+    reduction
+        One of "none", "mean", "sum", indicating whether to return one loss per data
+        point, the mean loss, or the sum of losses
+    Returns
+    -------
+    torch.Tensor
+        The calculated loss
+    Raises
+    ------
+    ValueError
+        If an invalid reduction keyword is submitted
+
+    Source: https://github.com/snorkel-team/snorkel/blob/master/snorkel/classification/loss.py
+    """
+
+    assert input.shape == target.shape, "Inputs and targets must have same shape!"
+
+    num_points, num_classes = input.shape
+    # Note that t.new_zeros, t.new_full put tensor on same device as t
+    cum_losses = input.new_zeros(num_points)
+    for y in range(num_classes):
+        target_temp = input.new_full((num_points,), y, dtype=torch.long)
+        y_loss = F.cross_entropy(input, target_temp, reduction="none")
+        if weight is not None:
+            y_loss = y_loss * weight[y]
+        cum_losses += target[:, y].float() * y_loss
+
+    if reduction == "none":
+        return cum_losses
+    elif reduction == "mean":
+        return cum_losses.mean()
+    elif reduction == "sum":
+        return cum_losses.sum()
+    else:
+        raise ValueError("Keyword 'reduction' must be one of ['none', 'mean', 'sum']")
 
 
 class FROMP:
