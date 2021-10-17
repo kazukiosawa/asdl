ae96e0e89e9a0d9e33ecf4c28dd7a55865de9ddf
kazukiosawa <osawa1021@gmail.com>
2021-10-17 10:29:29 +0200

    Add set_result() to Operation to simplify the registration of results & Mod to store op_result under Operation not under Module

--- asdfghjkl/operations/operation.py
+++ asdfghjkl/operations/operation.py
@@ -15,7 +15,7 @@ OP_ACCUMULATE_GRADS = 'acc_grad'  # accumulate gradients
 
 
 class Operation:
-    def __init__(self, module, model, op_names, save_field='op_results'):
+    def __init__(self, module, model, op_names):
         self._module = module
         self._model = model
         if isinstance(op_names, str):
@@ -23,18 +23,27 @@ class Operation:
         # remove duplicates
         op_names = set(op_names)
         self._op_names = op_names
-        self._save_field = save_field
         self._grads_scale = None
+        self.op_results = {}
 
-    def get_op_results(self):
-        return getattr(self._module, self._save_field, {})
-
-    def set_op_results(self, op_results):
-        setattr(self._module, self._save_field, op_results)
-
-    def clear_op_results(self):
-        if hasattr(self._module, self._save_field):
-            delattr(self._module, self._save_field)
+    def set_result(self, value, *keys):
+        """
+        Examples:
+             set_result(data, OP_COV_DIAG)
+             set_result(data, OP_BATCH_GRADS, 'weight')
+             set_result(A, OP_COV_KRON, 'A')
+        """
+        results = self.op_results
+        if len(keys) > 1:
+            for key in keys[:-1]:
+                if key not in results:
+                    results[key] = {}
+                results = results[key]
+        key = keys[-1]
+        if key in results:
+            results[key] += value
+        else:
+            results[key] = value
 
     @property
     def grads_scale(self):
@@ -59,11 +68,9 @@ class Operation:
                 ones = in_data.new_ones(shape)
                 in_data = torch.cat((in_data, ones), dim=1)
 
-            op_results = self.get_op_results()
-
             if OP_COV_KRON in self._op_names:
                 A = self.cov_kron_A(module, in_data)
-                op_results[OP_COV_KRON] = {'A': A}
+                self.set_result(A, OP_COV_KRON, 'A')
 
             if OP_GRAM_HADAMARD in self._op_names:
                 n_data = in_data.shape[0]
@@ -72,9 +79,7 @@ class Operation:
                     A = self.gram_A(module, in_data, in_data)
                 else:
                     A = self.gram_A(module, in_data[:n1], in_data[n1:])
-                op_results[OP_GRAM_HADAMARD] = {'A': A}
-
-            self.set_op_results(op_results)
+                self.set_result(A, OP_GRAM_HADAMARD, 'A')
 
     def backward_pre_process(self, in_data, out_grads):
         gs = self._grads_scale
@@ -87,24 +92,16 @@ class Operation:
                 out_grads.mul_(gs)
 
         module = self._module
-        op_results = self.get_op_results()
         for op_name in self._op_names:
             if op_name == OP_COV_KRON:
-                rst = self.cov_kron_B(module, out_grads)
-                assert op_name in op_results
-                if 'B' in op_results[op_name]:
-                    op_results[op_name]['B'] += rst
-                else:
-                    op_results[op_name]['B'] = rst
+                B = self.cov_kron_B(module, out_grads)
+                self.set_result(B, OP_COV_KRON, 'B')
 
             elif op_name == OP_COV_UNIT_WISE:
                 assert original_requires_grad(module, 'weight')
                 assert original_requires_grad(module, 'bias')
                 rst = self.cov_unit_wise(module, in_data, out_grads)
-                if op_name in op_results:
-                    op_results[op_name] += rst
-                else:
-                    op_results[op_name] = rst
+                self.set_result(OP_COV_UNIT_WISE, rst)
 
             elif op_name == OP_GRAM_HADAMARD:
                 n_data = in_data.shape[0]
@@ -113,7 +110,7 @@ class Operation:
                     B = self.gram_B(module, out_grads, out_grads)
                 else:
                     B = self.gram_B(module, out_grads[:n1], out_grads[n1:])
-                A = op_results[OP_GRAM_HADAMARD]['A']
+                A = self.op_results[OP_GRAM_HADAMARD]['A']
                 self._model.kernel += B.mul(A)
 
             elif op_name == OP_GRAM_DIRECT:
@@ -141,12 +138,10 @@ class Operation:
             else:
                 rst = getattr(self,
                               f'{op_name}_weight')(module, in_data, out_grads)
-                op_results[op_name] = {'weight': rst}
+                self.set_result(rst, op_name, 'weight')
                 if original_requires_grad(module, 'bias'):
                     rst = getattr(self, f'{op_name}_bias')(module, out_grads)
-                    op_results[op_name]['bias'] = rst
-
-        self.set_op_results(op_results)
+                    self.set_result(rst, op_name, 'bias')
 
     @staticmethod
     def batch_grads_weight(module, in_data, out_grads):
