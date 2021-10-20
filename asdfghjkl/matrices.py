bc1c87bed3306d9e56c9830108a7d5274d5ba754
kazukiosawa <osawa1021@gmail.com>
2021-10-20 20:03:12 +0200

    Add matrix_to_shapes_values() for mapping matrix shapes to some values

--- asdfghjkl/matrices.py
+++ asdfghjkl/matrices.py
@@ -1,7 +1,9 @@
 import os
 import copy
+import inspect
 
 import torch
+from torch.nn import Module
 import torch.distributed as dist
 
 from .symmatrix import SymMatrix
@@ -15,6 +17,7 @@ SHAPE_FULL = 'full'  # full
 SHAPE_BLOCK_DIAG = 'block_diag'  # layer-wise block-diagonal
 SHAPE_KRON = 'kron'  # Kronecker-factored
 SHAPE_DIAG = 'diag'  # diagonal
+ALL_SHAPES = [SHAPE_FULL, SHAPE_BLOCK_DIAG, SHAPE_KRON, SHAPE_DIAG]
 
 __all__ = [
     'MatrixManager',
@@ -25,13 +28,57 @@ __all__ = [
     'SHAPE_FULL',
     'SHAPE_BLOCK_DIAG',
     'SHAPE_KRON',
-    'SHAPE_DIAG'
+    'SHAPE_DIAG',
+    'matrix_shapes_to_values'
 ]
 
 _supported_types = [HESSIAN, FISHER_EXACT, FISHER_MC, FISHER_EMP]
 _supported_shapes = [SHAPE_FULL, SHAPE_BLOCK_DIAG, SHAPE_KRON, SHAPE_DIAG]
 
 _normalizations = (torch.nn.BatchNorm1d, torch.nn.BatchNorm2d)
+
+
+def matrix_shapes_to_values(matrix_shapes, shape_to_value_mapping, modules=None):
+    if isinstance(matrix_shapes, str):
+        matrix_shapes = [matrix_shapes]
+
+    def map_to_values(_shapes):
+        _values = [shape_to_value_mapping[_shape] for _shape in _shapes]
+        # remove duplicates
+        _values = list(set(_values))
+        return _values
+
+    modules_for = {shape: [] for shape in ALL_SHAPES}
+    if isinstance(matrix_shapes, list):
+        # common matrix_shapes for all modules
+        values = map_to_values(matrix_shapes)
+        if modules is not None:
+            for shape in ALL_SHAPES:
+                if shape in matrix_shapes:
+                    modules_for[shape] = modules
+    elif isinstance(matrix_shapes, dict):
+        values = {}
+        for key, shapes in matrix_shapes.items():
+            values[key] = map_to_values(shapes)
+            if modules is not None:
+                if isinstance(key, Module):
+                    # module-wise matrix_shapes
+                    for shape in ALL_SHAPES:
+                        if shape in shapes:
+                            modules_for[shape].append(key)
+                elif inspect.isclass(key) and issubclass(key, Module):
+                    # class-wise matrix_shapes
+                    for shape in ALL_SHAPES:
+                        if shape in shapes:
+                            modules_for[shape].extend(m for m in modules if isinstance(m, key))
+                else:
+                    raise TypeError(f'Invalid type of key in matrix_shapes: {type(key)}.')
+    else:
+        raise TypeError(f'Invalid type of matrix_shapes: {type(matrix_shapes)}.')
+
+    if modules is not None:
+        return values, modules_for
+    return values
 
 
 def _requires_matrix(module: torch.nn.Module):
