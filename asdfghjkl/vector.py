b6c402ddb6b55643e58afc8ea4e818b1f1497df1
kazukiosawa <osawa1021@gmail.com>
2021-11-16 14:37:34 +0100

    Mod params and vectors types in ParamVector

--- asdfghjkl/vector.py
+++ asdfghjkl/vector.py
@@ -10,19 +10,24 @@ class ParamVector:
         self.vectors: Dict[torch.Tensor, torch.Tensor] = {}
 
         if isinstance(vectors, torch.Tensor):
+            assert vectors.ndim == 1
             pointer = 0
-            for p in self.params:
+            for p in params:
                 numel = p.numel()
                 v = vectors[pointer: pointer + numel]
-                assert p.shape == v.shape
-                self.vectors[p] = v
+                self.vectors[p] = v.view_as(p)
                 pointer += numel
             assert pointer == vectors.numel()
-        else:
-            assert isinstance(vectors, dict)
-            for p in self.params:
-                assert p in vectors
+        elif isinstance(vectors, list):
+            for p, v in zip(params, vectors):
+                assert p.shape == v.shape
+                self.vectors[p] = v
+        elif isinstance(vectors, dict):
+            for p in params:
+                assert p.shape == vectors[p].shape
             self.vectors = vectors
+
+        raise TypeError(f'Invalid vectors type: {type(vectors)}')
 
     def __add__(self, other):
         assert self.params == other.params
@@ -46,7 +51,10 @@ class ParamVector:
 
     def get_vectors_by_module(self, module: nn.Module):
         params = [p for p in module.parameters()]
-        vectors = {p: self.vectors.get(p, None) for p in params}
+        return self.get_vectors_by_params(params)
+
+    def get_vectors_by_params(self, params: List[torch.Tensor]):
+        vectors = {p: self.vectors[p] for p in params if p in self.vectors}
         return ParamVector(params, vectors)
 
     def get_flatten_vector(self):
