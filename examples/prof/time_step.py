219dd83b7f1470afb3b3dd6bc504d6cbfb3a6ab4
kazukiosawa <osawa1021@gmail.com>
2021-12-02 13:10:10 +0100

    Mod to use mse_loss instead of cross_entropy

--- examples/prof/time_step.py
+++ examples/prof/time_step.py
@@ -3,10 +3,10 @@ import json
 import yaml
 
 import torch
-from torch.nn.functional import cross_entropy
+from torch.nn.functional import mse_loss
 import torchvision
 
-from asdfghjkl import KFAC, FISHER_EMP
+from asdfghjkl import KFAC, FISHER_EMP, LOSS_MSE
 from asdfghjkl import empirical_natural_gradient
 from asdfghjkl import LBFGS
 from asdfghjkl.precondition import Shampoo
@@ -16,8 +16,6 @@ import models
 
 # yapf: disable
 parser = argparse.ArgumentParser()
-parser.add_argument('--batch_size', type=int, default=64,
-                    help='input batch size')
 parser.add_argument('--input_size', type=str, default='32,32',
                     help='input size')
 parser.add_argument('--optim', type=str, default='kfac',
@@ -46,7 +44,7 @@ def time_sgd():
     def fwd():
         nonlocal loss
         with torch.autograd.profiler.emit_nvtx():
-            loss = cross_entropy(model(x), t)
+            loss = mse_loss(model(x), t)
 
     def bwd():
         with torch.autograd.profiler.emit_nvtx():
@@ -68,7 +66,7 @@ def time_adam():
     def fwd():
         nonlocal loss
         with torch.autograd.profiler.emit_nvtx():
-            loss = cross_entropy(model(x), t)
+            loss = mse_loss(model(x), t)
 
     def bwd():
         with torch.autograd.profiler.emit_nvtx():
@@ -90,7 +88,7 @@ def time_shampoo():
     def fwd():
         nonlocal loss
         with torch.autograd.profiler.emit_nvtx():
-            loss = cross_entropy(model(x), t)
+            loss = mse_loss(model(x), t)
 
     def bwd():
         with torch.autograd.profiler.emit_nvtx():
@@ -106,7 +104,7 @@ def time_shampoo():
 
 
 def time_kfac():
-    ng = KFAC(model, FISHER_EMP, damping=1.)
+    ng = KFAC(model, FISHER_EMP, loss_type=LOSS_MSE, damping=1.)
     optimizer = torch.optim.SGD(model.parameters(), lr=1)
 
     def fwd_bwd_upd_curv():
@@ -135,7 +133,7 @@ def time_smw():
 
     def fwd_bwd_precond():
         with torch.autograd.profiler.emit_nvtx():
-            empirical_natural_gradient(model, x, t, loss_fn=cross_entropy)
+            empirical_natural_gradient(model, x, t, loss_fn=mse_loss)
 
     def upd_param():
         with torch.autograd.profiler.emit_nvtx():
@@ -155,7 +153,7 @@ def time_lbfgs():
     def fwd():
         nonlocal loss
         with torch.autograd.profiler.emit_nvtx():
-            loss = cross_entropy(model(x), t)
+            loss = mse_loss(model(x), t)
 
     def bwd():
         with torch.autograd.profiler.emit_nvtx():
@@ -211,8 +209,8 @@ if __name__ == '__main__':
 
     # prepare data
     input_size = [int(s) for s in args.input_size.split(',')]
-    x = torch.rand(args.batch_size, *input_size).to(device)
-    t = torch.tensor([0] * x.size(0)).long().to(device)
+    x = torch.rand(*input_size).to(device)
+    t = torch.rand(*input_size).to(device)
 
     torch.cuda.reset_peak_memory_stats()
 
