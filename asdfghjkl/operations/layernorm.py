8caed0a934b0955ac69252ba0400ea96f36f04da
ishiihiro <ishii.h.aq@m.titech.ac.jp>
2021-12-30 21:17:42 +0900

    Added support for SHAPE_UNIT_WISE in LayerNorm
    Also fixed an issue where unnecessary in_data extention occurs.

--- asdfghjkl/operations/layernorm.py
+++ asdfghjkl/operations/layernorm.py
@@ -34,3 +34,41 @@ class LayerNorm(Operation):
     def cov_diag_bias(module, out_grads):
         return out_grads.mul(out_grads).sum(dim=0) # normalized_shape
     
+    @staticmethod
+    def cov_unit_wise(module, in_data, out_grads):
+        n_features = in_data.flatten(start_dim=1).shape[1] # (f[0] x f[1] x ... x f[-1])
+        grads_w = in_data.mul(out_grads) # n x normalized_shape
+        grads_b = out_grads # n x normalized_shape
+        cov_ww = (grads_w ** 2).sum(0).flatten() # n_features x 1
+        cov_bb = (grads_b ** 2).sum(0).flatten() # n_features x 1
+        cov_wb = (grads_w * grads_b).sum(0).flatten() # n_features x 1
+        blocks = torch.zeros(n_features, 2, 2).to(in_data.device)
+        for i in range(n_features):
+            blocks[i][0][0] = cov_ww[i]
+            blocks[i][1][1] = cov_bb[i]
+            blocks[i][0][1] = blocks[i][1][0] = cov_wb[i]
+        return blocks  # n_features x 2 x 2
+
+    @staticmethod
+    def cov_kron_A(module, in_data):
+        raise ValueError(
+            f'{OP_COV_KRON} operation is not supported in LayerNorm.'
+        )
+
+    @staticmethod
+    def cov_kron_B(module, out_grads):
+        raise ValueError(
+            f'{OP_COV_KRON} operation is not supported in LayerNorm.'
+        )
+
+    @staticmethod
+    def gram_A(module, in_data1, in_data2):
+        raise ValueError(
+            f'{OP_GRAM_HADAMARD} operation is not supported in LayerNorm.'
+        )
+
+    @staticmethod
+    def gram_B(module, out_grads1, out_grads2):
+        raise ValueError(
+            f'{OP_GRAM_HADAMARD} operation is not supported in LayerNorm.'
+        )
