a5d6a4c5051580db6a378784dfae579b9cee6ed3
kazukiosawa <osawa1021@gmail.com>
2022-02-03 08:06:15 +0100

    misc

--- asdfghjkl/operations/layernorm.py
+++ asdfghjkl/operations/layernorm.py
@@ -34,7 +34,7 @@ class LayerNorm(Operation):
     def batch_grads_weight(
         module: nn.Module, in_data: torch.Tensor, out_grads: torch.Tensor
     ):
-        return in_data.mul(out_grads) # n x normalized_shape
+        return in_data.mul(out_grads)  # n x normalized_shape
     
     @staticmethod
     def batch_grads_bias(module, out_grads):
@@ -43,7 +43,7 @@ class LayerNorm(Operation):
     @staticmethod
     def cov_diag_weight(module, in_data, out_grads):
         grads = in_data.mul(out_grads)
-        return grads.mul(grads).sum(dim=0) # normalized_shape
+        return grads.mul(grads).sum(dim=0)  # normalized_shape
     
     @staticmethod
     def cov_diag_bias(module, out_grads):
