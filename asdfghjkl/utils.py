5c33ff2e05082384169c238fef2e4d9d5d462ec7
Alexander Immer <alexndr.immer@gmail.com>
2022-05-04 13:23:24 +0200

    Minor change, use view not reshape

--- asdfghjkl/utils.py
+++ asdfghjkl/utils.py
@@ -64,7 +64,7 @@ def im2col_2d(x: torch.Tensor, conv2d: nn.Module):
 def im2col_2d_aug(x, conv2d):
     n, k_aug = x.shape[:2]
     x = im2col_2d(x.flatten(start_dim=0, end_dim=1), conv2d)
-    return x.reshape(n, k_aug, *x.shape[1:])
+    return x.view(n, k_aug, *x.shape[1:])
 
 
 def add_value_to_diagonal(x: torch.Tensor, value):
