5de3289844218861f7074b8e3f400a43b6fc819a
kazukiosawa <osawa1021@gmail.com>
2022-08-23 20:35:20 +0200

    Upd to use GradientMaker classes

--- tests/test_emirical_ngd.py
+++ tests/test_emirical_ngd.py
@@ -2,9 +2,11 @@ import copy
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-from asdfghjkl import FISHER_EMP
-from asdfghjkl import empirical_natural_gradient, FullNaturalGradientMaker
+from asdfghjkl import FISHER_EMP, SHAPE_FULL
+from asdfghjkl import NaturalGradientMaker, NaturalGradientMakerConfig
+from asdfghjkl import SmwEmpNaturalGradientMaker, SmwEmpNaturalGradientMakerConfig
 
 
 torch.random.manual_seed(0)
@@ -25,21 +27,31 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=batchsize, shuffle=
 
 damping = 1e-2
 model1 = copy.deepcopy(model)
-model2 = copy.deepcopy(model)
-ngd = FullNaturalGradientMaker(model1, fisher_type=FISHER_EMP, damping=damping)
 optim1 = torch.optim.SGD(model1.parameters(), lr=1)
+config = NaturalGradientMakerConfig(fisher_type=FISHER_EMP,
+                                    fisher_shape=SHAPE_FULL,
+                                    upd_curvature_interval=1,
+                                    upd_inv_interval=1,
+                                    damping=damping)
+nat_grad_maker = NaturalGradientMaker(model1, config)
+
+model2 = copy.deepcopy(model)
 optim2 = torch.optim.SGD(model2.parameters(), lr=1)
+config = SmwEmpNaturalGradientMakerConfig(damping=damping)
+smw_nat_grad_maker = SmwEmpNaturalGradientMaker(model2, config)
 
 for x, t in dataloader:
     x, t = x.to(device), t.to(device)
-    model1.zero_grad(set_to_none=True)
-    ngd.refresh_curvature(x, t, calc_emp_loss_grad=True)
-    ngd.update_inv()
-    ngd.precondition()
+    optim1.zero_grad(set_to_none=True)
+    dummy_y = nat_grad_maker.setup_model_call(model1, x)
+    nat_grad_maker.setup_loss_call(F.cross_entropy, dummy_y, t)
+    nat_grad_maker.forward_and_backward(scale=1/batchsize)
     optim1.step()
 
-    model2.zero_grad(set_to_none=True)
-    empirical_natural_gradient(model2, x, t, damping=damping)
+    optim2.zero_grad(set_to_none=True)
+    dummy_y = smw_nat_grad_maker.setup_model_call(model1, x)
+    smw_nat_grad_maker.setup_loss_call(F.cross_entropy, dummy_y, t)
+    smw_nat_grad_maker.forward_and_backward(data_size=batchsize)
     optim2.step()
 
     for p1, p2 in zip(model1.parameters(), model2.parameters()):
