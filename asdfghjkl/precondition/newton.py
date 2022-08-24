62399f7e64f05dd7cf2f23061d06b88e8bc2d550
kazukiosawa <osawa1021@gmail.com>
2022-08-24 14:41:49 +0200

    Add NewtonGradientMaker

---
+++ asdfghjkl/precondition/newton.py
@@ -0,0 +1,54 @@
+from typing import Tuple, Union, Any
+from dataclasses import dataclass
+
+import torch
+from torch import Tensor
+from torch.nn.utils import parameters_to_vector, vector_to_parameters
+from ..grad_maker import GradientMaker
+from ..hessian import HessianMaker, HessianMakerConfig
+from ..matrices import SHAPE_FULL
+
+
+__all__ = ['NewtonGradientMakerConfig', 'NewtonGradientMaker']
+
+
+@dataclass
+class NewtonGradientMakerConfig:
+    damping: float = 1.e-8
+    absolute: bool = False
+
+
+class NewtonGradientMaker(GradientMaker):
+    def __init__(self, model, config):
+        assert isinstance(config, NewtonGradientMakerConfig)
+        super().__init__(model)
+        self.config = config
+        hessian_config = HessianMakerConfig(hessian_shapes=[SHAPE_FULL])
+        self.hessian_maker = HessianMaker(model, hessian_config)
+
+    def forward_and_backward(self,
+                             data_size=1,
+                             scale=1.,
+                             accumulate=False
+                             ) -> Union[Tuple[Any, Tensor], Any]:
+        self.delegate_forward_and_backward(self.hessian_maker,
+                                           data_size=data_size,
+                                           scale=scale,
+                                           accumulate=accumulate)
+        self.precondition()
+        if self._loss_fn is None:
+            return self._model_output
+        else:
+            return self._model_output, self._loss
+
+    def precondition(self):
+        hessian = self.model.hessian.data
+        grads = [p.grad for p in self.model.parameters() if p.grad is not None]
+        if self.config.absolute:
+            L, Q = torch.linalg.eigh(hessian)
+            hessian = Q @ torch.abs(torch.diag(L)) @ Q.T
+        diag = torch.diagonal(hessian)
+        diag += self.config.damping
+        g = parameters_to_vector(grads)
+        vector_to_parameters(torch.linalg.solve(hessian, g), grads)
+
